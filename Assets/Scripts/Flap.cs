b7f1979abc486b8993cf9b650c4652bce63bd4a4
Boyan Botev <boyanbotev@gmail.com>
2025-04-22 12:43:39 +0300

    feat: basic flap movement

---
+++ Assets/Scripts/Flap.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Flap : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float flapForce = 5f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.linearVelocity = Vector2.up * flapForce;
+        }
+    }
+}
